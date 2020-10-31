7a3987b2e02cf41e39dafa468a6d4bd9f08e0346
aknn <anil.kumar.nn@gmail.com>
2020-10-30 19:45:48 -0500

    added comment

--- paran.py
+++ paran.py
@@ -1,5 +1,12 @@
 
-print('I am:', __name__)
+"""
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+"""
 def balparant(s):
 
     stack=[]
