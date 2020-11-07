95c5003d218d30cac3d146bb3a972146129a0fa4
aknn <anil.kumar.nn@gmail.com>
2020-11-07 00:42:29 -0600

    initial

---
+++ hindex.py
@@ -0,0 +1,20 @@
+"""
+The h-index is a metric that attempts to measure the productivity and citation impact of the publication of a scholar. The definition of the h-index is if a scholar has at least h of their papers cited h times.
+
+Given a list of publications of the number of citations a scholar has, find their h-index.
+
+
+"""
+
+
+def hindex(lis=[ ]):
+    if len(lis) == 0:
+        return 0
+    for j in range(max(lis), -1, -1):
+        check = [ x - j for x in lis ]
+        PosCount = len(list(filter(lambda x: (x >= 0), check)))
+        if j <= PosCount:
+            return j
+
+
+print(hindex())
