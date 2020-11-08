558671eea06882e3d04e2f4754e4c24416c73feb
aknn <anil.kumar.nn@gmail.com>
2020-11-08 01:30:24 -0600

    second

--- revvowels.py
+++ revvowels.py
@@ -8,14 +8,13 @@ def reverse_vowel(s):
     vowels = "AEIOUaeiou"
     i, j = 0, len(s) - 1
     s = list(s)
+
     while i < j:
-        print(f'first while {i, j}')
         while i < j and s[ i ] not in vowels:
             i += 1
-            print(f'sec while i {i}')
         while i < j and s[ j ] not in vowels:
             j -= 1
-            print(f'third while j {i, j}')
+
         s[ i ], s[ j ] = s[ j ], s[ i ]
         i, j = i + 1, j - 1
     return "".join(s)
