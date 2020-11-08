db52625f628a54beda70384645ae4c9cc3e17843
aknn <anil.kumar.nn@gmail.com>
2020-11-08 01:28:44 -0600

    initial

---
+++ revvowels.py
@@ -0,0 +1,24 @@
+"""
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+"""
+
+
+def reverse_vowel(s):
+    vowels = "AEIOUaeiou"
+    i, j = 0, len(s) - 1
+    s = list(s)
+    while i < j:
+        print(f'first while {i, j}')
+        while i < j and s[ i ] not in vowels:
+            i += 1
+            print(f'sec while i {i}')
+        while i < j and s[ j ] not in vowels:
+            j -= 1
+            print(f'third while j {i, j}')
+        s[ i ], s[ j ] = s[ j ], s[ i ]
+        i, j = i + 1, j - 1
+    return "".join(s)
+
+
+print(reverse_vowel('adele'))
