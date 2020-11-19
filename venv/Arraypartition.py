cdbcfc3c4043f19acd0ae28d807ea7aaf763dbd9
aknn <anil.kumar.nn@gmail.com>
2020-11-18 23:32:59 -0600

    initial

---
+++ venv/Arraypartition.py
@@ -0,0 +1,14 @@
+
+"""
+
+Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn)
+such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+
+"""
+
+def arrayPairSum(nums) -> int:
+    nums.sort()
+    tot = 0
+    for i in range(0, len(nums), 2):
+        tot += nums[ i ]
+    return tot
