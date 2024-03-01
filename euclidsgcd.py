9f65b88d615f6fd00dffc034ae8052fe1ad1790f
anilkumar <anil.kumar.nn@gmail.com>
2024-03-01 15:33:01 +0000

    more pythonic

--- euclidsgcd.py
+++ euclidsgcd.py
@@ -2,8 +2,7 @@
 euclids algorithm to find gcd
 
 """
-def gcd(a,b):
-    print(a,b)
-    if b==0:
-        return a
-    return gcd(b,a%b)
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
